34e9710f1c1b1936129c58fe917f2cd588bfffd8
Heger Arias Santos <hegel.0218@gmail.com>
2022-08-06 19:33:39 -0400

    init

---
+++ TalendingBarbershop.API/TalendingBarbershop.API/Controllers/V1/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TalendingBarbershop.Data.Responses;
+using TalendingBarbershop.Services.Orders;
+
+namespace TalendingBarbershop.API.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<RequestResult>> Get()
+        {
+            return await 
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- TalendingBarbershop.API/TalendingBarbershop.Services/Orders/Orders.cs
+++ TalendingBarbershop.API/TalendingBarbershop.Services/Orders/Orders.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TalendingBarbershop.Data.Responses;
 
 namespace TalendingBarbershop.Services.Orders
 {
     public class Orders
     {
+        public static async RequestResult Get() { 
+            RequestResult result = new RequestResult();
+            result.Data = new Dictionary<string, object>();
+            result.Response = true;
+            result.Message = "Lista completa";
+
+            return result;
+        }
     }
 }
