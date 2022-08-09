0e782385bc17e852601e64731e01194c80b79947
RonnyAraujoTest <102432892+RonnyAraujoTest@users.noreply.github.com>
2022-08-08 22:44:12 -0400

    agregando Roles Default

--- TalendingBarbershop.API/TalendingBarbershop.API/Startup.cs
+++ TalendingBarbershop.API/TalendingBarbershop.API/Startup.cs
@@ -29,7 +29,7 @@ namespace TalendingBarbershop.API
         {
             services.AddDbContext<DbTalendigBarbershopContext>(option =>
             {
-                option.UseSqlServer(Configuration.GetConnectionString("DbTalendigBarbershop"));
+                option.UseSqlServer(Configuration.GetConnectionString("DefaultConnetion"));
             });
             services.AddControllers();
         }
