80ac5512bb9adaf1bf96c7b41e41e10dec34acdc
MiguelAngel00 <108034293+Miguel00Angel@users.noreply.github.com>
2022-08-10 16:16:23 -0400

    Quotes

---
+++ TalendingBarbershop.API/TalendingBarbershop.API/Controllers/V1/QuoteController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TalendingBarbershop.Data.DTOs;
+using TalendingBarbershop.Data.Responses;
+using TalendingBarbershop.Services.Quotes;
+
+namespace TalendingBarbershop.API.Controllers.V1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuoteController : Controller
+    {
+        private IQuotes _quote;
+        private RequestResult _requestResult;
+
+        [HttpPost("new-quote")]
+        public async Task<ActionResult<RequestResult>> AddQuote([FromBody] TblQuotesDTO quote)
+        {
+            _requestResult = await _quote.AddQuote(quote);
+            return _requestResult;
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPut("edit-quote")]
+        public async Task<ActionResult<RequestResult>> EditUser([FromBody]int id, TblQuotesDTO quote)
+        {
+            _requestResult = await _quote.EditQuote(id, quote);
+            return _requestResult;
+        }
+    }
+}
--- TalendingBarbershop.API/TalendingBarbershop.Services/Quotes/Quotes.cs
+++ TalendingBarbershop.API/TalendingBarbershop.Services/Quotes/Quotes.cs
@@ -1,10 +1,90 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TalendingBarbershop.Data.DTOs;
+using TalendingBarbershop.Data.Models;
+using TalendingBarbershop.Data.Responses;
+using TalendingBarbershop.Data.Security;
 
 namespace TalendingBarbershop.Services.Quotes
 {
-    public class Quotes
+    public class Quotes : IQuotes
     {
+        private IMapper _mapper;
+        private RequestResult _requestResult;
+        private TblQuotes _quotes;
+        private DbTalendigBarbershopContext _dbContext;
+
+        public Quotes(IMapper mapper)
+        {
+            _mapper = mapper;
+            _requestResult = new RequestResult();
+            _dbContext = new DbTalendigBarbershopContext();
+            _quotes = new TblQuotes();
+        }
+
+        public async Task<RequestResult> AddQuote(TblQuotesDTO quote)
+        {
+            try 
+            { 
+                _quotes = _mapper.Map<TblQuotes>(quote);
+                await _dbContext.TblQuotes.AddAsync(_quotes);
+                await _dbContext.SaveChangesAsync();
+
+                _requestResult.Message = "Exito";
+                _requestResult.Data = "La cita fue agregado con exito.";
+                _requestResult.Response = true;
+
+            } catch(Exception ex) 
+            {
+                _requestResult.Message = ex.Message;
+                _requestResult.Data = null;
+                _requestResult.Response = false;
+            }
+
+            return _requestResult;
+        }
+
+        public async Task<RequestResult> EditQuote(int id,TblQuotesDTO quote)
+        {
+            
+            _quotes = _mapper.Map<TblQuotes>(quote);
+            _quotes.Id = id;
+            _dbContext.Entry(_quotes).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                _requestResult.Message = "Exito";
+                _requestResult.Data = "La cita fue modificada con exito.";
+                _requestResult.Response = true;
+            }
+            catch (Exception ex)
+            {
+                _requestResult.Message = ex.Message;
+                _requestResult.Data = null;
+                _requestResult.Response = false;
+            }
+
+            return _requestResult;
+        }
+
+       public Task<RequestResult> DeleteQuote(int id)
+       {
+            try
+            {
+                if ()
+                {
+
+                }
+            } catch(Exception ex)
+            {
+
+            }
+            return null;
+       }
     }
 }
